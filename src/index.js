b7a006bc9c0afad6d96bd767da443a2e107a5716
ryoku70 <zaydaddaoudi12@gmail.com>
2026-09-04 15:39:35 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const Discord = require('discord.js');
 const chalk = require('chalk');
-require('dotenv').config('./.env');
+require('dotenv').config({ path: './.env' });
 const axios = require('axios');
 const webhook = require("./config/webhooks.json");
 const config = require("./config/bot.js");
